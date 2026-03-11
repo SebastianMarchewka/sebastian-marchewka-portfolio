abf887fa9831ea81f350a830632e59b83b68374f
SebastianMarchewka <sebamarchewka@hotmail.com>
2026-03-11 12:14:32 -0400

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const message = document.getElementById("formMessage");
-    message.textContent = "Thank you for your message. I will respond shortly.";
-});
